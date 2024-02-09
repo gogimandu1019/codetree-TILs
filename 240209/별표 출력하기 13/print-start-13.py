6b92073251f05807f23a2992a5222bab3b52fa05
ms.k <35169281+gogimandu1019@users.noreply.github.com>
2024-02-09 18:40:44 +0900

    Title: 별표 출력하기 13, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 240209/별표 출력하기 13/print-start-13.py
+++ 240209/별표 출력하기 13/print-start-13.py
@@ -1,32 +1,12 @@
+# 변수 선언 및 입력
 n = int(input())
 
-plus = 1
-minus = n
-
-for i in range(1, n+1):
-    if i % 2 == 0:
-        print('* '*plus)
-        plus += 1
-    else:
-        print('* '*minus)
-        minus -= 1
-
-plus = (n//2 + 1)
-minus = n//2
-
-if n % 2 == 0:
-    for j in range(1, n +1):
-        if j % 2 == 0:
-            print('* '*plus )
-            plus += 1
-        else:
-            print('* '*minus)
-            minus-= 1
-else:
-    for k in range(1, n+1):
-        if k % 2 == 0:
-            print('* '*minus)
-            minus -= 1
-        else:
-            print('* '*plus)
-            plus += 1
+# i가 홀수인 경우 별을 1 + (i // 2)개, 짝수인 경우 n - (i // 2)개 출력합니다
+for i in range(2 * n):
+	if i % 2 == 1:
+		for _ in range(1 + i // 2):
+			print("* ", end="")
+	else:
+		for _ in range(n - i // 2):
+			print("* ", end="")
+	print()
