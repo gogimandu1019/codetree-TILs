a4bf70136d176f932587e16dbf5c37f6c030f092
ms.k <35169281+gogimandu1019@users.noreply.github.com>
2024-02-09 18:40:07 +0900

    Title: 별표 출력하기 13, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240209/별표 출력하기 13/print-start-13.py
+++ 240209/별표 출력하기 13/print-start-13.py
@@ -1,16 +1,32 @@
 n = int(input())
 
-for i in range(1, n):
-    if i == 1:
-        for _ in range (n):
-            print("*", end = " ")
-        print()
-    elif i == 2:
-        print("*", end = " ")
-        print()
-    else :
-        for j in range(1, n-1):
-            for k in range(n-j):
-                print("*", end = " ")
-            print()
-print()
+plus = 1
+minus = n
+
+for i in range(1, n+1):
+    if i % 2 == 0:
+        print('* '*plus)
+        plus += 1
+    else:
+        print('* '*minus)
+        minus -= 1
+
+plus = (n//2 + 1)
+minus = n//2
+
+if n % 2 == 0:
+    for j in range(1, n +1):
+        if j % 2 == 0:
+            print('* '*plus )
+            plus += 1
+        else:
+            print('* '*minus)
+            minus-= 1
+else:
+    for k in range(1, n+1):
+        if k % 2 == 0:
+            print('* '*minus)
+            minus -= 1
+        else:
+            print('* '*plus)
+            plus += 1
