f4a6ebb9696a3faf53565e87c2093a5b61700565
ms.k <35169281+gogimandu1019@users.noreply.github.com>
2024-02-18 19:49:08 +0900

    Title: 알파벳 지우기, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240218/알파벳 지우기/remove-alphabet.py
+++ 240218/알파벳 지우기/remove-alphabet.py
@@ -1,9 +1,20 @@
 str1 = list(input())
 str2 = list(input())
 x = ""
+y = ""
+
 for i in range(len(str1)):
-    if int(str1[i]) >= 0 and int(str1[i]) <= 9:
-        x = ''.join(str1[i])
-        print(x)
+    if str.isdigit(str1[i]) == True:
+        x = x + ''.join(str1[i])
     else :
         continue;
+
+
+for i in range(len(str2)):
+    if str.isdigit(str2[i]) == True:
+        y = y + ''.join(str2[i])
+    else :
+        continue;
+
+
+print(x+y)
