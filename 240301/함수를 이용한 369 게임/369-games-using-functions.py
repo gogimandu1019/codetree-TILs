6e15dde07d2ce432350d548cda3debd65344feb7
ms.k <35169281+gogimandu1019@users.noreply.github.com>
2024-03-01 23:07:31 +0900

    Title: 함수를 이용한 369 게임, Time: 201ms, Memory: 25MB, Status: Passed - CodeTree

--- 240301/함수를 이용한 369 게임/369-games-using-functions.py
+++ 240301/함수를 이용한 369 게임/369-games-using-functions.py
@@ -2,9 +2,7 @@ a,b = map(int, input().split())
 cnt = 0
 
 def having369(x):
-    k = x // 10
-    l = x % 10
-    if k == 3 or k == 6 or k == 9 or l == 3 or l == 6 or l == 9 or x % 3 == 0:
+    if '3' in str(x) or '6' in str(x) or '9' in str(x) or x % 3 == 0:
         return 1
     else:
         return 0
