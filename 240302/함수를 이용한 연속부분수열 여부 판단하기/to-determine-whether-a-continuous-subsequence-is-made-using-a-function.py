594069d0e290f8df579c21a7cca3517bd726b2bf
ms.k <35169281+gogimandu1019@users.noreply.github.com>
2024-03-02 17:59:10 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240302/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
+++ 240302/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -1,9 +1,12 @@
-n1, n2 = map(int, input().split())
-A = list(map(int, input().split()))
-B = list(map(int, input().split()))
+# 변수 선언 및 입력:
+n1, n2 = tuple(map(int, input().split()))
+
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
 
 # n번째 인덱스부터 시작하는 n2길이의 a수열과
-# b수열이 완전히 일치하는지 확인
+# b수열이 완전히 일치하는지 확인합니다..
 def is_same(n):
     for i in range(n2):
         if a[i + n] != b[i]:
@@ -12,7 +15,7 @@ def is_same(n):
     return True
 
 
-# b가 a의 연속부분수열인지 확인
+# b가 a의 연속부분수열인지 확인합니다.
 def is_subsequence():
     for i in range(n1 - n2 + 1):
         if is_same(i):
