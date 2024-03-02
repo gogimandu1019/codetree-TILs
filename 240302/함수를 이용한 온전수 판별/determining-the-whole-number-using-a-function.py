0a12441b0816545986e66e9e0733e53ea42420fd
ms.k <35169281+gogimandu1019@users.noreply.github.com>
2024-03-02 16:12:34 +0900

    Title: 함수를 이용한 온전수 판별, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240302/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
@@ -0,0 +1,21 @@
+a, b = map(int, input().split())
+perfect_cnt = 0
+
+def chkPerfect(x):
+    if x % 2 == 0: #2로나누어떨어짐
+        return 0
+    else:
+        if x % 5 == 0 and x % 10 != 0: #일의자리 5 : 5로는 떨어지고 10으론 안떨어짐
+            return 0
+        else:
+            if x % 3 == 0 and x % 9 != 0:   #3으로 나누어 떨어지는데 9로는 나누어떨어지지 않음
+                return 0
+            else:
+                return 1
+
+
+for i in range(a, b+1):
+    perfect_cnt += chkPerfect(i)
+
+
+print(perfect_cnt)
