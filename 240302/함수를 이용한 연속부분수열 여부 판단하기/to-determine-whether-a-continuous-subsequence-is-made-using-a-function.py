04a08781b7e6a096de2099763316c2f45abed832
ms.k <35169281+gogimandu1019@users.noreply.github.com>
2024-03-02 17:18:33 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240302/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
+++ 240302/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -4,24 +4,29 @@ B = list(map(int, input().split()))
 
 def chkContinuePart(x,y,Z,Q):
     flag = False
+
     for i in range(y):
-        if Z[i] == Q[0]:
-            for j in range(1, y):
-                if Z[i] == Q[j]:
+        if x < y:
+            flag = False
+            break;
+        else:
+            if Z[i] == Q[0]:
+                for j in range(1, y):
+                    if Z[i] == Q[j]:
+                        i += 1
+                        flag = True
+                        continue;
+                    else:
+                        flag = False
+                        break;
+            else:
+                if i < x:
                     i += 1
-                    flag = True
                     continue;
                 else:
                     flag = False
                     break;
-        else:
-            if i < x:
-                i += 1
-                continue;
-            else:
-                flag = False
-                break;
-    
+        
     if flag == False:
         print("No")
     elif flag == True:
