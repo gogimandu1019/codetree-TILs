19641633c4566737d50dff9ed3136bbd8005b67e
ms.k <35169281+gogimandu1019@users.noreply.github.com>
2024-03-02 17:55:27 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240302/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
+++ 240302/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -2,35 +2,26 @@ n1, n2 = map(int, input().split())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
-def chkContinuePart(x,y,Z,Q):
-    flag = False
+# n번째 인덱스부터 시작하는 n2길이의 a수열과
+# b수열이 완전히 일치하는지 확인
+def is_same(n):
+    for i in range(n2):
+        if a[i + n] != b[i]:
+            return False
 
-    for i in range(x):
-        if x < y:
-            flag = False
-            break;
-        else:
-            if Z[i] == Q[0]:
-                for j in range(0, y):
-                    if Z[i] == Q[j]:
-                        flag = True
-                        i += 1
-                        j += 1
-                        continue;
-                    else:
-                        break;
-            else:
-                if i < x:
-                    i += 1
-                else:
-                    flag = False
-                    break;
-        
-    if flag == False:
-        print("No")
-    elif flag == True:
-        print("Yes")
+    return True
 
 
+# b가 a의 연속부분수열인지 확인
+def is_subsequence():
+    for i in range(n1 - n2 + 1):
+        if is_same(i):
+            return True
+    
+    return False
 
-chkContinuePart(n1,n2,A,B)
+
+if is_subsequence():
+    print("Yes")
+else:
+    print("No")
