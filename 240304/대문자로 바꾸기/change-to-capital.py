242bef61fc8e13d23bd4a0a514f8efd505f3b0e9
ms.k <35169281+gogimandu1019@users.noreply.github.com>
2024-03-04 19:00:24 +0900

    Title: 대문자로 바꾸기, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 240304/대문자로 바꾸기/change-to-capital.py
+++ 240304/대문자로 바꾸기/change-to-capital.py
@@ -1,8 +1,7 @@
 arr = [list(input().split()) for _ in range(5)]
-answer = [[0,0,0]] * 5
 
 for i in range(5):
     for j in range(3):
-        answer[i][j] = arr[i][j].upper()
-        print(answer[i][j], end = ' ')
+        arr[i][j] = arr[i][j].upper()
+        print(arr[i][j], end = ' ')
     print()
