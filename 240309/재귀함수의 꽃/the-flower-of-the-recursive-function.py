b27150b9e3c6a558c893047b0264aec189ae385f
ms.k <35169281+gogimandu1019@users.noreply.github.com>
2024-03-09 20:53:22 +0900

    Title: 재귀함수의 꽃, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240309/재귀함수의 꽃/the-flower-of-the-recursive-function.py
@@ -0,0 +1,23 @@
+N = int(input())
+
+def recursive_print_down(x):
+    if x == 0:
+        return
+    print(x, end = ' ')
+    recursive_print_down(x-1)
+
+def recursive_print_up(x):
+    if x == 0:
+        return
+    
+    recursive_print_up(x-1)
+    
+    if x == N:
+        print(x)
+    else:
+        print(x, end = ' ')
+
+
+
+recursive_print_down(N)
+recursive_print_up(N)
