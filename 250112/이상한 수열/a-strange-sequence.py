fbfef60e89858e60740ea1d56d3718023dd1f472
ms.k <35169281+gogimandu1019@users.noreply.github.com>
2025-01-12 11:34:00 +0900

    Title: 이상한 수열, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250112/이상한 수열/a-strange-sequence.py
+++ 250112/이상한 수열/a-strange-sequence.py
@@ -3,11 +3,11 @@ N = int(input())
 # Write your code here!
 def f(x):
     y = 0
-    if x = 1:
+    if x == 1:
         y = 1
-    elif  x = 2 :
+    elif  x == 2:
         y = 2
-    else :
+    else:
         y = f(int(x/3)) + f(x-1)
     
     return y
