1a60c155e1702b4570957470c8aeeba63d15d417
ms.k <35169281+gogimandu1019@users.noreply.github.com>
2025-03-16 22:52:34 +0900

    Title: 10개 입력받기, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 250316/10개 입력받기/receive-10-inputs.py
+++ 250316/10개 입력받기/receive-10-inputs.py
@@ -3,12 +3,15 @@ n = len(arr)
 summ = 0
 avg = 0
 k = 0
+j = 0
 for i in range(n):
     if arr[i] == 0:
         k = i
         break;
+    else :
+        summ += arr[i]
+        k = k + 1
+if k != 0:
+    avg = round(summ / k, 1)
     
-    summ += arr[i]
-
-avg = round(summ / k, 1)
 print(f'{summ} {avg}')
